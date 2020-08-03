cb3680be1e0eab152ef4d79c2c087733280e7290
Naveen Kashyap <naveenkashyap243@gmail.com>
2020-08-03 20:33:23 +0530

    ✨UPDATE: Get orders which payment is complete

--- api/routes/order.js
+++ api/routes/order.js
@@ -4,10 +4,13 @@ const authenticateUser = require("../middlewares/authenticateUser");
 
 router.get("/", authenticateUser, async (req, res) => {
   const body = req.body;
-  const orders = await Orders.find({ user_id: body.user_id });
+  const orders = await Orders.find({
+    user_id: body.user_id,
+    payment_status: 1,
+  });
   if (orders.length > 0) {
     res.status(200).json({
-      orders: [orders],
+      orders,
     });
   } else {
     res.status(404).json({
