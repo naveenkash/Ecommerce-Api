3c9e4f45bd3a4c813fb34f0e78d5eb68226f352c
Naveen Kashyap <naveenkashyap243@gmail.com>
2020-08-09 21:39:34 +0530

    ADD route to get user feedback for single product

--- api/routes/feedback.js
+++ api/routes/feedback.js
@@ -5,12 +5,22 @@ const CartItems = require("../models/cartItem");
 const Feedbacks = require("../models/feedback");
 const authenticateUser = require("../middlewares/authenticateUser");
 const apiError = require("../error-handler/apiErrors");
+
+/**
+ * @param {stars number}
+ * @param {product_id string}
+ * @param {feedback string} optional
+ */
 router.post(
   "/create",
   authenticateUser,
   CheckIfItemBought,
   async (req, res, next) => {
     const body = req.body;
+    if (!body.stars || body.stars > 5 || body.stars < 1) {
+      next(apiError.badRequest("Stars atleast must be 1 or max 5"));
+      return;
+    }
     const session = await mongoose.startSession();
     session.startTransaction();
     try {
@@ -68,16 +78,41 @@ router.post(
   }
 );
 
+/**
+ * @param {product_id string}
+ */
+router.get(
+  "/:productId",
+  authenticateUser,
+  CheckIfItemBought,
+  async (req, res, next) => {
+    const productId = req.params.productId;
+    const body = req.body;
+    try {
+      const feedback = await Feedbacks.findOne({
+        product_id: productId,
+        user_id: body.user_id,
+      });
+      if (!feedback) {
+        next(apiError.badRequest("Cannot found product with specified id"));
+        return;
+      }
+      res.status(200).json({
+        feedback,
+      });
+    } catch (error) {
+      next(apiError.interServerError(error.message));
+      return;
+    }
+  }
+);
+
 function convertToInt(num) {
   return parseInt(num);
 }
 
 async function CheckIfItemBought(req, res, next) {
   const body = req.body;
-  if (body.stars > 5 || body.stars < 1) {
-    next(apiError.badRequest("Stars atleast must be 1 or max 5"));
-    return;
-  }
   const itemBought = await CartItems.findOne({
     user_id: body.user_id,
     product_id: body.product_id,
