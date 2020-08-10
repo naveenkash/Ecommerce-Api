2852f85686887c29bd090b560564015edda3553a
Naveen Kashyap <naveenkashyap243@gmail.com>
2020-08-10 23:45:45 +0530

    create jwt when user login

--- api/routes/login.js
+++ api/routes/login.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const Users = require("../models/user");
 const apiError = require("../error-handler/apiErrors");
 const sha256 = require("js-sha256").sha256;
+const jwt = require("jsonwebtoken");
 
 router.get("/local", async (req, res, next) => {
   const body = req.body;
@@ -12,7 +13,7 @@ router.get("/local", async (req, res, next) => {
       const savedPassword = user.password;
       if (typedPassword == savedPassword) {
         let userObj = {
-          _id: user._id,
+          user_id: user._id,
           name: user.name,
           lastname: user.lastname,
           email: user.email,
@@ -20,8 +21,23 @@ router.get("/local", async (req, res, next) => {
           display_name: user.display_name,
           img: user.img,
         };
+
+        const token = jwt.sign(
+          {
+            user_id: new_user._id,
+            name: new_user.name,
+            lastname: new_user.lastname,
+            email: new_user.email,
+            img: new_user.img,
+            created_at: new_user.created_at,
+            display_name: new_user.display_name,
+          },
+          process.env.JWT_ACCESS_TOKEN_SECERET
+        );
+
         res.status(200).json({
           user: userObj,
+          token,
         });
         return;
       } else {
